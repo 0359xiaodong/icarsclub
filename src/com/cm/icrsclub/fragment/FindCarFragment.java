f63bf14861a1c4067dedb729e860a445acf60972
cmad <feng_33191@126.com>
2014-07-21 14:08:47 +0800

    添加了find car界面的头部

--- src/com/cm/icrsclub/fragment/FindCarFragment.java
+++ src/com/cm/icrsclub/fragment/FindCarFragment.java
@@ -13,11 +13,16 @@ import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class FindCarFragment extends Fragment implements OnClickListener {
 	private RelativeLayout rl_find_car_map;
 	private RelativeLayout rl_find_car;
+	private RelativeLayout rl_title;
+	private ImageView img_title;
+	private TextView title_city;
 	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
@@ -32,6 +37,11 @@ public class FindCarFragment extends Fragment implements OnClickListener {
 		View view = inflater.inflate(R.layout.fragment_find_car,container,false);
 		rl_find_car_map = (RelativeLayout) view.findViewById(R.id.find_car_map);
 		rl_find_car = (RelativeLayout) view.findViewById(R.id.find_car);
+		rl_title = (RelativeLayout) view.findViewById(R.id.rl_title);
+		img_title = (ImageView) view.findViewById(R.id.chang_city_img);
+		title_city =(TextView) view.findViewById(R.id.city);
+		
+		rl_title.setOnClickListener(this);
 		rl_find_car.setOnClickListener(this);
 		rl_find_car_map.setOnClickListener(this);
 		return view;
@@ -50,6 +60,9 @@ public class FindCarFragment extends Fragment implements OnClickListener {
 		case R.id.find_car_map:
 			rl_find_car.startAnimation(getTranslateAnimationup());
 			rl_find_car.setVisibility(View.VISIBLE);
+			break;
+		case R.id.rl_title:
+			
 			break;
 		}
 		
---
+++ src/com/cm/icrsclub/popup/ChangCityPopup.java
@@ -0,0 +1,10 @@
+package com.cm.icrsclub.popup;
+
+import android.widget.PopupWindow;
+
+public class ChangCityPopup extends PopupWindow {
+	
+	public ChangCityPopup(){
+		
+	}
+}
